d9f9dd99e8c1031cb0df71e633f6bdf81ae24cb9
Ekiles225 <ekiles07@outlook.com>
2024-06-03 10:45:29 -0500

    operacionsuma

---
+++ JS/myJS.js
@@ -0,0 +1,11 @@
+var numero1 = document.getElementById("numero1").value;
+var numero2 = document.getElementById("numero2").value;
+
+
+
+function opercaionSuma(){
+    const resultado = parseInt(numero1) + parseInt(numero2);
+    alert("El resultado es: " + resultado);
+}
+
+
